fa6e1a3263b4ffc08e8839f41cd4ac05b74fc58c
Matthew Skillman <mskillman@trek10.com>
2020-10-20 16:06:23 -0400

    add git-related outputs to stack

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
+GIT_REPO="$(git config --get remote.origin.url)"
+GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+GIT_COMMIT_ID="$(git rev-parse --short HEAD)"
+export GIT_COMMIT_ID GIT_BRANCH GIT_REPO
+
 npm install
 
 aws cloudformation package \
@@ -14,6 +19,9 @@ aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --stack-name ${STACK_NAME} \
   --parameter-overrides \
+    GitRepo="${GIT_REPO}" \
+    GitBranch="${GIT_BRANCH}" \
+    GitCommitId="${GIT_COMMIT_ID}" \
     DefaultRoleName=${DEFAULT_ROLE_NAME} \
     SecretsManagerPrefix=${SECRETS_MANAGER_PREFIX} \
     BucketName=${BUCKET_NAME} \
